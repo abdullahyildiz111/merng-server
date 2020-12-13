042e04bcf0b251a4e4743d18c5fac00413ab4235
abdullahyildiz111 <abdullahyildiz111@gmail.com>
2020-12-13 13:30:05 +0300

    ***

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGODB :'mongodb+srv://abdullah:EoeXILNLh2cdnlgK@cluster0.a4ekx.mongodb.net/merng-deneme?retryWrites=true&w=majority',
+    SECRET_KEY: 'very top secret key'
+};
+
+ 
